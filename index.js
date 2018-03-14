5239a3d8d9a2bffc3b23c2e6f59e9ba776189b01
Marian <marian.urbanczyk@spiritlink.de>
2018-03-14 11:23:42 +0100

    + remove extension option + pretty use file extension + compress option

--- index.js
+++ index.js
@@ -5,23 +5,24 @@
  * Licensed under the MIT License.
  */
 
-var union = require('arr-union');
-var xml = require('jstoxml');
-var path = require('path');
-var zlib = require('zlib');
-var fs = require('fs');
+let union = require('arr-union');
+let js2xml = require('jstoxml');
+let xml2js = require('xml2js');
+let path = require('path');
+let zlib = require('zlib');
+let fs = require('fs');
 
 module.exports = function(params, cb) {
-    var assemble = params.assemble;
-    var grunt = params.grunt;
-    var pages = assemble.options.pages;
-    var len = pages.length;
-    var options = assemble.options.sitemap || {};
-    var sitemap = [];
-    var robots = [];
-    var exclusion = ['404'];
-    var pkg = grunt.file.readJSON('package.json');
-    
+    let assemble = params.assemble;
+    let grunt = params.grunt;
+    let pages = assemble.options.pages;
+    let len = pages.length;
+    let options = assemble.options.sitemap || {};
+    let sitemap = [];
+    let robots = [];
+    let exclusion = ['404'];
+    let pkg = grunt.file.readJSON('package.json');
+
     options.homepage = options.homepage || pkg.homepage;
     options.robot = options.robot !== false;
     options.changefreq = options.changefreq || 'weekly';
@@ -31,16 +32,17 @@ module.exports = function(params, cb) {
     options.basename = options.basename || 'sitemap.xml';
     options.removeExt = options.removeExt || false;
     options.compress = options.compress || true;
-    
+    options.update = options.update || false;
+
     if (typeof options.exclude !== 'undefined') {
         exclusion = union([], exclusion, options.exclude || []);
     }
-    
+
     // Only write if it actually changed.
-    var write = function(file, content) {
-        var msg;
-        var old = grunt.file.exists(file) ? grunt.file.read(file) : '';
-        
+    let write = function(file, content) {
+        let msg;
+        let old = grunt.file.exists(file) ? grunt.file.read(file) : '';
+
         if (old !== content) {
             grunt.file.write(file, content);
             msg = 'Created '.yellow + file.cyan;
@@ -49,12 +51,12 @@ module.exports = function(params, cb) {
         }
         return grunt.verbose.ok(msg);
     };
-    
+
     // Return the relative destination if the option is enabled
-    var getExternalFilePath = function(relativedest, file, removeExt) {
-        var finalFilename = file.dest;
-        var fileExtension = path.extname(file.dest)
-        
+    let getExternalFilePath = function(relativedest, file, removeExt) {
+        let finalFilename = file.dest;
+        let fileExtension = path.extname(file.dest);
+
         if (relativedest === true) {
             relativedest = options.dest;
         }
@@ -62,29 +64,62 @@ module.exports = function(params, cb) {
             finalFilename = finalFilename.substring(0, finalFilename.length - fileExtension.length);
         }
         if (options.pretty === true) {
-            var index = 'index' + (removeExt === true ? '' : fileExtension);
+            let index = 'index' + (removeExt === true ? '' : fileExtension);
             if(finalFilename.lastIndexOf(index) === finalFilename.length - index.length) {
                 finalFilename = finalFilename.substring(index, finalFilename.length - index.length);
             }
         }
         return (relativedest ? finalFilename.replace(relativedest + '/', '') : finalFilename);
     };
-    
-    var url = options.homepage;
-    var relativedest = options.relativedest;
-    
-    for (var i = 0; i < len; i++) {
-        var file = pages[i];
-        var date = file.data.updated || file.data.date || new Date();
-        var changefreq = file.data.changefreq || options.changefreq;
-        var priority = file.data.priority || options.priority;
-        
+
+    let url = options.homepage;
+    let relativedest = options.relativedest;
+
+    let sitemapDest = options.dest + '/' + options.basename;
+
+    console.log(`options.update ${sitemapDest} ${options.update}`);
+    if(options.update)
+    {
+        let parser = new xml2js.Parser();
+        try
+        {
+            let temp = fs.readFileSync(sitemapDest, "utf8");
+            parser.parseString(temp, function (err, result)
+            {
+                if(result && result.urlset && result.urlset.url)
+                {
+
+                    for(let i = 0; i < result.urlset.url.length; i++)
+                    {
+                        let url = result.urlset.url[i];
+                        let obj = {};
+                        for(let a in url)
+                        {
+                            obj[a] = url[a].toString();
+                        }
+                        sitemap.push({
+                            url : obj
+                        });
+                    }
+                }
+            });
+        } catch (err) {
+            console.log(`error ${sitemapDest} ${err}`);
+        }
+    }
+
+    for (let i = 0; i < len; i++) {
+        let file = pages[i];
+        let date = file.data.updated || file.data.date || new Date();
+        let changefreq = file.data.changefreq || options.changefreq;
+        let priority = file.data.priority || options.priority;
+
         if (exclusion.indexOf(file.basename) !== -1 ||
             grunt.file.isMatch({srcBase: options.dest}, exclusion, file.dest)) {
             robots.push('Disallow: /' + getExternalFilePath(relativedest, file, false));
             continue;
         }
-        
+
         sitemap.push({
             url: {
                 loc: url + '/' + getExternalFilePath(relativedest, file, options.removeExt),
@@ -94,32 +129,32 @@ module.exports = function(params, cb) {
             }
         });
     }
-    
-    var result = xml.toXML({
+
+    let result = js2xml.toXML({
         _name: 'urlset',
         _attrs: {
             xmlns: 'http://www.sitemaps.org/schemas/sitemap/0.9'
         },
         _content: sitemap
     }, {header: true, indent: '  '});
-    
-    var sitemapDest = options.dest + '/' + options.basename;
+
+
     write(sitemapDest, result);
-    
+
     if (options.robot) {
-        var sitemapFile = {dest: url + '/' + sitemapDest};
-        var robot = 'User-agent: *\n';
-        
+        let sitemapFile = {dest: url + '/' + sitemapDest};
+        let robot = 'User-agent: *\n';
+
         robot += robots.join('\n') + '\n\n';
-        
+
         robot += 'Sitemap: ' + getExternalFilePath(relativedest, sitemapFile, false);
         robot += '\n';
-        
-        var robotpDest = options.dest + '/robots.txt';
+
+        let robotpDest = options.dest + '/robots.txt';
         write(robotpDest, robot);
     }
     if (options.compress) {
-        var compressor = zlib.createGzip({level: 1});
+        let compressor = zlib.createGzip({level: 1});
         compressor.pipe(fs.createWriteStream(sitemapDest + '.gz'));
         compressor.end(result);
     }
